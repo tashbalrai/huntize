1322517fa3a334e5985d18b825ed823811a674c4
Vipan Balrai <vipan.balrai@gmail.com>
2024-01-08 03:46:34 +0000

    Added layout, loading and colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'body-white': 'F5F7FA',
+        'body-blue': '#0760A8',
+        'body-black': '#000000',
+        'body-orange': '#E85941',
+        'card-bg': '#D9D9D9',
+        'line':'#E3E2E2',
+        'text-black': '#000000',
+        'text-yellow': '#EDEF4E',
+      }
     },
   },
   plugins: [],
