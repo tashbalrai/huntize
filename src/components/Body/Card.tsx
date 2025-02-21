7683055d7d7d44971e4e845fd3b26ad00314f9da
vipan balrai <vipan.balrai@gmail.com>
2025-02-21 04:36:18 +0000

    Added animation on text and card

--- src/components/Body/Card.tsx
+++ src/components/Body/Card.tsx
@@ -3,13 +3,20 @@ import { FaShareAlt } from "react-icons/fa";
 import type { ICard } from "../../utils/types";
 import Box from "./Box";
 import { useBoxAttributes } from "../../utils/hooks/UseBoxAttributes";
+import SplitText from "./Text";
+import { useState } from "react";
 
 const Card = ({ data }: ICard) => {
     const { width } = useBoxAttributes();
+    const [isPlaying, setIsPlaying] = useState(false);
 
     return (
         <Box height="h-article-height" width={`${width / 16}rem`}>
-            <article className="group flex flex-col h-full px-5 pt-11 pb-6 justify-between hover:bg-(--hover-bg-card-color) hover:cursor-pointer ">
+            <article
+                onMouseEnter={() => setIsPlaying(true)}
+                onMouseLeave={() => setIsPlaying(false)}
+                className="group flex flex-col h-full px-5 pt-11 pb-6 justify-between hover:bg-(--hover-bg-card-color) hover:cursor-pointer hover:scale-105 transition-transform duration-300"
+            >
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-row w-full justify-between">
                         <h6>/ {data.category}</h6>
