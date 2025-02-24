eacddedd1d89aba11e01d905e92dc97755bfa21e
vipan balrai <vipan.balrai@gmail.com>
2025-02-24 01:55:58 +0000

    Added base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: "https://tashbalrai.github.io",
+    base: "/huntize/",
     integrations: [react()],
     vite: {
         plugins: [tailwindcss()],
